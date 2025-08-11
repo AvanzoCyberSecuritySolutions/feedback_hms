d819d0be544deb64f17429c857c482510d8f70e7
akhi0216 <akhilam21@gmail.com>
2025-08-11 14:48:10 +0530

    default url changed

--- lib/services/app_utils.dart
+++ lib/services/app_utils.dart
@@ -13,7 +13,8 @@ class AppUtils {
   //     defaultValue: "http://192.168.1.28:8000/hmsapp/api");
 
   // naveen
-  static const String pythonBaseURL = String.fromEnvironment("BASE_URL",
-    defaultValue: "https://hmsdevenv.highlandhospitals.in/api/hmsapp/api",
+  static const String pythonBaseURL = String.fromEnvironment(
+    "BASE_URL",
+    defaultValue: "http://192.168.220.61:8000/hmsapp/api",
   );
 }
